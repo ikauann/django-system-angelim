89c3a2bd8f6fc5cbd679a3e4ce56c542189bf84b
Kauan Souza <72037450+ikauann@users.noreply.github.com>
2022-01-22 13:48:24 -0300

    adicionando o metodo post

--- bling/serializers.py
+++ bling/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import BlingEstoque
-from rest_framework.response import Response
 
 
 class BlingEstoqueSerializar(serializers.ModelSerializer):
@@ -9,8 +8,3 @@ class BlingEstoqueSerializar(serializers.ModelSerializer):
 
         model = BlingEstoque
         fields = '__all__'
-
-    def fb_credits_callback(request):
-        # parse with your parse function
-        # handle request
-        return HttpResponse(json.dumps(data))
--- bling/views.py
+++ bling/views.py
@@ -1,17 +1,16 @@
-import copy, json, datetime
-from django.utils import timezone
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-
 from rest_framework import generics
 from .models import BlingEstoque
 from .serializers import BlingEstoqueSerializar
 
+#from rest_framework.response import Response
+#from rest_framework import status
+
+#from django.http import QueryDict
+
 
 class BlingEstoqueList(generics.ListCreateAPIView):
 
-    queryset = BlingEstoque.objects.all()
+    queryset = BlingEstoque.objects.all()[:10]
     serializer_class = BlingEstoqueSerializar
 
     def post(self, request):
