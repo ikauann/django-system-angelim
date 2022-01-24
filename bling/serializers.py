9ca8fe52d7a131a9997c174c7c10b7d61bba8d4c
Kauan Souza <72037450+ikauann@users.noreply.github.com>
2022-01-24 12:30:27 -0300

    serializer.save()

--- bling/serializers.py
+++ bling/serializers.py
@@ -1,8 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 from .models import BlingEstoque
 
 
 class BlingEstoqueSerializar(serializers.ModelSerializer):
+
+    sku = serializers.IntegerField(primary_key=True)
+    estoque = serializers.CharField(max_length=300)
 
     class Meta:
 
