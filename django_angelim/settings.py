bc3d1a7d713252c636b9ee40c832f0031c0c4adb
Kauan Souza <72037450+ikauann@users.noreply.github.com>
2022-01-24 10:26:49 -0300

    arrumando as settings

--- django_angelim/settings.py
+++ django_angelim/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import django_heroku
 #heroku
 
-django_heroku.settings(locals(), staticfiles=False)
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
