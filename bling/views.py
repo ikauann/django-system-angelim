7aa82723affa1f37a0ba79f3fad822550b1bb5ab
Kauan Souza <72037450+ikauann@users.noreply.github.com>
2022-01-24 12:22:03 -0300

    serializer.save()

--- bling/views.py
+++ bling/views.py
@@ -26,10 +26,10 @@ class BlingEstoqueList(generics.ListCreateAPIView):
         data_post = tratamento_qj(data_post)
 
         serializer = BlingEstoqueSerializar(data=data_post)
+        print(serializer)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            print(Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK))
         else:
-            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-            print(serializer.errors)
+            print(Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST))
 
