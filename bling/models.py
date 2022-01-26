c36743d4dd54094203e6915c6582aadc7672f683
Kauan Souza <72037450+ikauann@users.noreply.github.com>
2022-01-26 12:36:41 -0300

    adicionando no banco de dados

--- bling/models.py
+++ bling/models.py
@@ -5,5 +5,5 @@ class BlingEstoque(models.Model):
 
     objects = models.Manager()
 
-    sku = models.CharField(max_length=300, primary_key=False)
+    sku = models.CharField(max_length=300, primary_key=True)
     estoque = models.CharField(max_length=300)
