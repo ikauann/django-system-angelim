5c8243a6bd580ea8310f5dd71ee3478a1eeb7b5f
Kauan Souza <72037450+ikauann@users.noreply.github.com>
2022-01-26 12:53:00 -0300

    adicionando no banco de dados

--- bling/serializers.py
+++ bling/serializers.py
@@ -13,5 +13,5 @@ class BlingEstoqueSerializar(serializers.ModelSerializer):
     def create(self, validated_data):
         obj, created = BlingEstoque.objects.update_or_create(
         sku=14444423, estoque='Lennon',
-        defaults={'sku': 1591},
+        defaults={'sku': 14444423},
         )
